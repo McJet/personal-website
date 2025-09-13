d85b01c970842cc773662f857077ad6349acf99e
Jet Ysalina <jethercy@gmail.com>
2025-09-12 23:15:51 -0700

    fixed images

--- src/features/homepage/projects.js
+++ src/features/homepage/projects.js
@@ -3,7 +3,7 @@ import { newTabSvg } from "./svgs";
 const projects = [
   {
     title: "Saddleback.com",
-    image: "/src/features/homepage/assets/saddleback.png",
+    image: "/saddleback.png",
     span: "Aug 2023 - Current",
     description:
       "I am currently working for Saddleback Church as a Full-Stack Web Developer. Maintaining and building features within the website are a few of the things that I do on a daily basis.",
@@ -39,7 +39,7 @@ const projects = [
   },
   {
     title: "ATM.com",
-    image: "/src/features/homepage/assets/atm.jpg",
+    image: "/atm.jpg",
     span: "Feb 2022 - Oct 2022",
     description:
       "My professional developer journey starts at ATM.com, a fintech startup company. The company's product was a financial app in both iOS and Android. I worked on developing features within the Android side of things.",
